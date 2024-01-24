b337ee3cd217db0c1159e2ca5eb1174814f867b9
Parthsharma004 <134805985+Parthsharma004@users.noreply.github.com>
2024-01-24 20:06:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -82,16 +82,16 @@ clean = {}
 autoclean = []
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://te.legra.ph/file/0c373d33859cd378dcafe.jpg"
+    "START_IMG_URL", "https://telegra.ph/file/16bf52bbc1268b34296bc.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://graph.org/file/f16f757fe9c10f81b67ef.jpg"
+    "PING_IMG_URL", "https://telegra.ph/file/9407f732a405fa31463bc.jpg"
 )
-PLAYLIST_IMG_URL = "https://te.legra.ph/file/e68f585a0a2e4e5dc1164.jpg"
-STATS_IMG_URL = "https://te.legra.ph/file/4a7c28726502e24ea0fe0.jpg"
-TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/810f874873e1565cf5732.jpg"
-TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/16d7dd76f4ce8b8b01fdf.jpg"
-STREAM_IMG_URL = "https://te.legra.ph/file/53f1a295e172d39eaa39d.jpg"
+PLAYLIST_IMG_URL = "https://telegra.ph/file/e8e43916e090cb7868daf.jpg"
+STATS_IMG_URL = "https://telegra.ph/file/dcf3b7d87ed7f4d8db036.jpg"
+TELEGRAM_AUDIO_URL = "https://telegra.ph/file/dcf3b7d87ed7f4d8db036.jpg"
+TELEGRAM_VIDEO_URL = "https://telegra.ph/file/dcf3b7d87ed7f4d8db036.jpg"
+STREAM_IMG_URL = "https://telegra.ph/file/e8e43916e090cb7868daf.jpg"
 SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
 YOUTUBE_IMG_URL = "https://te.legra.ph/file/53f1a295e172d39eaa39d.jpg"
 SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/5d90c3bc7f0d229194a9f.jpg"
